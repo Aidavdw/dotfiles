ddf5f07c167cac46f51b1a0dfa3193da9c37e5b2
Aidavdw <39270007+Aidavdw@users.noreply.github.com>
2025-06-10 12:32:09 +0200

    fix(engineering scripts) Fix labels

--- engineering-scripts/mesh_convergence_plot.py
+++ engineering-scripts/mesh_convergence_plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 # Simply plots the mesh convergence.
 
 # First, the value you are trying to check the error of, at your highest resolution case:
-f_ref: float = 800
+f_ref: float = 1005.02
 
 # Add here your normalised grid spacings:
 # (The distance between two cell centers)
@@ -16,7 +16,7 @@ normalised_grid_spacings: list[float] = [2.0, 4.0, 8.0]
 
 # Fill in some 'metric' here for all your normalised grid spacings. This is a thing you want to minimise the error of.
 # e.g. temperature, current density, whatever.
-f_metric: list[float] = [802.0, 806.0, 820.0]
+f_metric: list[float] = [1005.14, 1005.56, 1007.06]
 
 
 ## END OF INPUT SEGMENT ##
@@ -24,7 +24,7 @@ f_metric: list[float] = [802.0, 806.0, 820.0]
 
 # Usually denoted as epsilon.
 def rel_err(f_n: float, f_ref: float) -> float:
-    return (f_n - f_ref) / f_ref
+    return abs((f_n - f_ref) / f_ref)
 
 
 # Calculate the relative change in value over a mesh change
@@ -32,7 +32,7 @@ rel_changes = [rel_err(f_n, f_ref) for f_n in f_metric]
 
 
 _ = plt.loglog(normalised_grid_spacings, rel_changes, marker="o", linestyle="-")
-_ = plt.xlabel("X Axis (log scale)")
-_ = plt.ylabel("Y Axis (log scale)")
+_ = plt.xlabel("N (normalised grid spacing)")
+_ = plt.ylabel("$\\varepsilon$ (relative error)")
 _ = plt.grid(True, which="both", ls="--")
 _ = plt.show()
