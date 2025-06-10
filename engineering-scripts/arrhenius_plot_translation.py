0d2fa8e8903cd289d9407c3a200c118212f50b6b
Aidavdw <39270007+Aidavdw@users.noreply.github.com>
2025-06-10 12:46:01 +0200

    fix(engineering scripts) Arrhenius plot use x directly
    Easier to ctrl-v from other sources

--- engineering-scripts/arrhenius_plot_translation.py
+++ engineering-scripts/arrhenius_plot_translation.py
@@ -2,27 +2,27 @@ from math import exp
 # Fuck you if you use arrhenius plots.
 # Convert them to actually humanly readable plots
 
-
 # The horizontal axis: 1000 / T in kelvin.
-xs = [
+x = [
     0.8517241379310345,
-    0.9308316430020285,
-    1.0281947261663287,
-    1.1438133874239351,
+    0.93184584178499,
+    1.0271805273833672,
+    1.1448275862068966,
     1.2939148073022313,
-    1.4866125760649087,
+    1.4845841784989857,
     1.7452332657200813,
 ]
 
+
 # The vertical axis: ln(σ T)
-ys = [
-    12.550387596899224,
-    12.465116279069768,
-    12.322997416020673,
-    12.180878552971578,
-    12.038759689922482,
-    11.811369509043928,
-    11.527131782945737,
+y = [
+    2.801033591731267,
+    1.692506459948321,
+    0.4134366925064601,
+    -1.064599483204134,
+    -2.883720930232558,
+    -5.441860465116279,
+    -8.710594315245478,
 ]
 
 
@@ -37,17 +37,15 @@ def invert_vertical_axis(y: float, temperature_in_kelvin: float) -> float:
 
 
 if __name__ == "__main__":
-    if len(ys) != len(xs):
+    if len(y) != len(x):
         print("Not same amount of entries in X and Y axis.")
 
     temperatures: list[float] = []
     sigmas: list[float] = []
 
-    for i in range(len(ys)):
-        x = xs[i]
-        temperature_in_kelvin = fix_horizontal_axis(x)
-        y = ys[i]
-        sigma = invert_vertical_axis(y, temperature_in_kelvin)
+    for i in range(len(y)):
+        temperature_in_kelvin = fix_horizontal_axis(x[i])
+        sigma = invert_vertical_axis(y[i], temperature_in_kelvin)
         temperatures.append(temperature_in_kelvin)
         sigmas.append(sigma)
 
