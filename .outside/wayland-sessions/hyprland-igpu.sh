fffbafa6fcf5e388d7108d14699c2a1c1028dc79
Aida van de Wetering <aida@osphire.com>
2025-10-07 13:16:55 +0200

    update wayland sessions for dgpu

--- .outside/wayland-sessions/hyprland-igpu.sh
+++ .outside/wayland-sessions/hyprland-igpu.sh
@@ -1,7 +1,31 @@
 # Specifically run hyprland on the igpu.
-# This will be set by the udev rules.
+# Also set up every other program to launch under the igpu,
+# unless specifically started using the dgpu with e.g. prime-run
+# or my own wrapper script.
 # Then set GL vendor etc to make sure all apps start on igpu
 # except if explicitly called with prime-run
+
+# Which specific gpu device to run hyprland on.
+# the device is a symlink to the igpu, set up from udev rules. 
+# (see the other modules in this git)
+export AQ_DRM_DEVICES=/dev/dri/intel-igpu
+# OpenGL: used by hyprland itself too!
+export __GLX_VENDOR_LIBRARY_NAME=mesa
 # libva driver is the intel VA-API.
-AQ_DRM_DEVICES=/dev/dri/intel-igpu __GLX_VENDOR_LIBRARY_NAME=mesa LIBVA_DRIVER_NAME=iHD Hyprland
+export LIBVA_DRIVER_NAME=iHD
+export VDPAU_DRIVER=va_gl
+
+export KWIN_DRM_DEVICES=/dev/dri/intel-igpu
+
+# Attempt at stopping apps from activating the dgpu for like 5 seconds
+# from https://bbs.archlinux.org/viewtopic.php?pid=2098846#p2098846
+# Extended with my own findings, already put on arch wiki.
+# These must be overwritten when you try to run something on the nvidia gpu!
+export __EGL_VENDOR_LIBRARY_FILENAMES=/usr/share/glvnd/egl_vendor.d/50_mesa.json
+# For nvidia: /usr/share/glvnd/egl_vendor.d/10_nvidia.json
+export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/intel_icd.x86_64.json
+# For nvidia: /usr/share/vulkan/icd.d/nvidia_icd.json
+
+
+Hyprland
 
--- .outside/wayland-sessions/hyprland-nvidia-gpu.sh
+++ .outside/wayland-sessions/hyprland-nvidia-gpu.sh
@@ -3,4 +3,7 @@
 # AQ_DRM_DEVICES=/dev/dri/nvidia-gpu Hyprland
 # and then recommended flags by hyprland wiki for nvidia GPU
 # https://wiki.hypr.land/Nvidia/
-LIBVA_DRIVER_NAME=nvidia __GLX_VENDOR_LIBRARY_NAME=nvidia Hyprland
+# Don't need to set __EGL_CENDOR_LIBRARY_FILENAMES and VK_ICD_FILENAMES, as the nvidia gpu is turned on anyway. Doesn't save waking it up.
+LIBVA_DRIVER_NAME=nvidia \
+__GLX_VENDOR_LIBRARY_NAME=nvidia \
+Hyprland
