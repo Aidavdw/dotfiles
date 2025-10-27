8a1772a0bdae13e73c16707beb5658d34baf5082
Aida van de Wetering <aida@osphire.com>
2025-10-27 17:17:45 +0100

    which-key devicons

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,6 +2,7 @@ return {
     -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VeryLazy', -- Sets the loading event to 'VimEnter'
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
       -- delay between pressing a key and opening which-key (milliseconds)
       -- this setting is independent of vim.opt.timeoutlen
