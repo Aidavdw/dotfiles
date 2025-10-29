93bc1f05e4ef4066cc5d7e5f69fb785554c73d88
Aida van de Wetering <aida@osphire.com>
2025-10-29 14:08:59 +0100

    allow code actions through fzf

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -3,6 +3,11 @@ return {
     -- optional for icon support
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {},
+    config = function(_, opts)
+        local fzf = require('fzf-lua')
+        fzf.setup(opts)
+        fzf.register_ui_select()
+    end,
     keys = {
         -- [F]ind pickers. These is the broadest level.
         {
