5497cba86686c7d0295ca12642ac8212e2af51c1
Aida van de Wetering <aida@osphire.com>
2025-10-29 08:53:31 +0100

    add all lsps I use

--- lua/06-lsp.lua
+++ lua/06-lsp.lua
@@ -1,5 +1,23 @@
--- We don't need lspconfig in the traditional sense anymore
--- Use native instead.
--- https://boltless.me/posts/neovim-config-without-plugins-2025/
--- Files are loaded from the ../lsp directory
-vim.lsp.enable('lua_ls')
+-- We don't need lspconfig in the traditional sense any more.
+-- However, it still gives nice presets for lsp configurations.
+-- so for LSPs where we just want to use a 'default' configuration
+-- we can just 'enable()' here and that's it.
+-- you can still customise it though, for which you'd place a file in the
+-- ../lsp directory.
+-- https://0xunicorn.com/neovim-native-lsp-config/
+vim.lsp.enable({
+    'lua_ls',
+    'clangd',
+    'cmake',
+    'cssls',
+    -- Fortran
+    'fortls',
+    'html',
+    'texlab',
+    -- Python static type checker
+    'basedpyright',
+    -- Python linter
+    'ruff',
+    -- Obsidian-like markdown
+    'markdown-oxide',
+})
