a10ba8be7491dd4e3ce4f64340a89ef43aff0a21
Aida van de Wetering <aida@osphire.com>
2025-10-29 17:28:17 +0100

    add indent blankline, integrated with rainbow delimiters

---
+++ lua/plugins/rainbow-delimiters.lua
@@ -0,0 +1,26 @@
+-- [(())] different colours for different pairs of brackets!
+return {
+    "hiphish/rainbow-delimiters.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+    },
+
+    -- This plugin also has a .setup() function,
+    -- while actually storing its info directly in vim.g.rainbow_delimiters
+    -- as it is merely a TreeSitter extension.
+    opts = {
+        highlight = {
+            "RainbowDelimiterRed",
+            "RainbowDelimiterYellow",
+            "RainbowDelimiterBlue",
+            "RainbowDelimiterOrange",
+            "RainbowDelimiterGreen",
+            "RainbowDelimiterViolet",
+            "RainbowDelimiterCyan",
+        },
+    },
+    config = function(_, opts)
+        require("rainbow-delimiters.setup").setup(opts)
+    end,
+}
