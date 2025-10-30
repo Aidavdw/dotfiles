e32708546f623724d8152808ce6be8de14066d49
Aida van de Wetering <aida@osphire.com>
2025-10-30 08:23:38 +0100

    continue list autocommand

--- lua/03-autocommands.lua
+++ lua/03-autocommands.lua
@@ -10,4 +10,22 @@ vim.api.nvim_create_autocmd("TextYankPost", {
         vim.highlight.on_yank()
     end,
 })
--- vim: ts=2 sts=2 sw=2 et
+
+-- Automatically continue lists when inserting a new line
+-- bullets.vim is too much,
+-- and autolist.nvim is too buggy.
+-- Modified from https://www.reddit.com/r/neovim/comments/1bis4h3/comment/kvmfjka
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "markdown", "tex", "text" },
+    callback = function()
+        vim.opt_local.formatoptions:append("r") -- `<CR>` in insert mode
+        vim.opt_local.formatoptions:append("o") -- `o` in normal mode
+        vim.opt_local.comments = {
+            "b:- [ ]", -- tasks
+            "b:- [x]",
+            "b:*", -- unordered list
+            "b:-",
+            "b:+",
+        }
+    end,
+})
