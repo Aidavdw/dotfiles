2f7d6d5414379bd6e15bbb467a52cad5294eaa67
Aida van de Wetering <aida@osphire.com>
2025-10-31 09:58:57 +0100

    move snippets to lua folder
    Otherwise they are not on the path

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -29,8 +29,8 @@ return {
         -- load snippets, with support for multiple profiles using NVIM_APPNAME
         local appname = vim.env.NVIM_APPNAME or "nvim"
         local paths = {
-            string.format("%s/.config/%s/snippets", vim.fn.expand("~"), appname),
-            string.format("%s/.config/nvim/snippets", vim.fn.expand("~")), -- fallback/global snippets
+            string.format("%s/.config/lua/%s/snippets", vim.fn.expand("~"), appname),
+            string.format("%s/.config/nvim/lua/snippets", vim.fn.expand("~")), -- fallback/global snippets
         }
         require("luasnip.loaders.from_lua").lazy_load({ paths = paths })
     end,
