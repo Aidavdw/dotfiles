fc0312bacf2a5605564dcdc126b22d004d0e23b3
Aida van de Wetering <aida@osphire.com>
2025-10-31 14:06:30 +0100

    fix luasnip snippet dir

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -29,7 +29,7 @@ return {
         -- load snippets, with support for multiple profiles using NVIM_APPNAME
         local appname = vim.env.NVIM_APPNAME or "nvim"
         local paths = {
-            string.format("%s/.config/lua/%s/snippets", vim.fn.expand("~"), appname),
+            string.format("%s/.config/%s/lua/snippets", vim.fn.expand("~"), appname),
             string.format("%s/.config/nvim/lua/snippets", vim.fn.expand("~")), -- fallback/global snippets
         }
         require("luasnip.loaders.from_lua").lazy_load({ paths = paths })
