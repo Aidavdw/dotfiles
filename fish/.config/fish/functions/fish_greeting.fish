f740997be3796a3f9cbf703dbdaa1aaff7d6c712
Aida van de Wetering <aida@osphire.com>
2026-01-12 16:45:37 +0100

    conditionally print other stuff around schedule too

--- fish/.config/fish/functions/fish_greeting.fish
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -1,11 +1,10 @@
 function fish_greeting --description 'What is displayed every time you first open the terminal'
-    echo fish session(set_color yellow)$USER(set_color normal)@(set_color blue)$hostname(set_color normal) 
-
-    echo Schedule:
     # If khal is installed, show the schedule
     if type -q khal
+        echo Schedule:
         khal list now +2d
-
+        echo -------------------------------------------------
+    else
+        echo fish session: (set_color yellow)$USER(set_color normal)@(set_color blue)$hostname(set_color normal) 
     end
-    echo -------------------------------------------------
 end
