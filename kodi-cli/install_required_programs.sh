c65945cdc3fbbadfd8a05869942ede654f0a3ec8
Aida van de Wetering <aida@osphire.com>
2026-02-02 10:35:51 +0100

    script for playing youtube videos on kodi htpc

--- kodi-cli/install_required_programs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p /$HOME/.program_source_code
-git clone https://github.com/nawar/kodi-cli /$HOME/.program_source_code 
-mkdir -p /$HOME/scripts
-ln -s /$HOME/.program_source_code /$HOME/scripts/kodi-cli
-chmod +x /$HOME/kodi-cli
