e19cdf47329d7f346516844a43476b229e9969a0
Aida van de Wetering <aida@osphire.com>
2026-02-24 15:38:48 +0100

    keyboard toggle update

---
+++ fish/.config/fish/conf.d/graphite-keyboard.fish
@@ -0,0 +1,6 @@
+# Some of the keybinds do not work very well because of funny keyboard layout,
+# potentially without alt key (IBM model M).
+# So, remap!
+
+bind \ce edit_command_buffer
+bind -M insert \ce edit_command_buffer
