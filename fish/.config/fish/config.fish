474e0dee9c99183c4758e268d5e91a3e2659a21a
Aida van de Wetering <aida@osphire.com>
2026-03-04 08:29:09 +0100

    use colours in less abbr

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -18,3 +18,9 @@ abbr -a --position command journalctl 'journalctl --b 0'
 # GREP by default is case-sensitive.
 # Whenever we look for stuff manually, we probably want to find all.
 abbr -a grep 'grep -i'
+
+# -R – the RAW_CONTROL_CHARS, can therefore show colours.
+# -i – searches within less are case-insensitive
+# -X – don’t clear the screen after using less
+# -j5 – put the result of searches on the fifth line of the screen where possible
+abbr -a less "less -RiXj5"
