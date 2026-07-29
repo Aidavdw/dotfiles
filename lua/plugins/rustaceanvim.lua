e5e450f2c348e889376fca88af7c1fd252bf2a49
Aida van de Wetering <aida@osphire.com>
2026-07-29 16:13:03 +0200

    set more rustaceanvim settings

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -3,8 +3,5 @@ return {
     "mrcjkb/rustaceanvim",
     version = "^6", -- Recommended
     lazy = false, -- This plugin is already lazy,
-    ["rust-analyzer"] = {
-        cargo = { allFeatures = true },
-    },
     -- BUG: luasnip does not play nice with the way how blink.cmp integration works for this plugin.
 }
