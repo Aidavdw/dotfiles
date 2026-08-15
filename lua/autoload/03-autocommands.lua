3279a1448713980d49b42d1ad144929f6a65309a
Aida van de Wetering <aida@osphire.com>
2026-08-15 17:35:51 +0200

    move yank highlight autocommand to visual

--- lua/autoload/03-autocommands.lua
+++ lua/autoload/03-autocommands.lua
@@ -1,16 +1,5 @@
 -- Autocommands
 
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight when yanking (copying) text",
-    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-})
-
 -- Automatically continue lists when inserting a new line
 -- bullets.vim is too much,
 -- and autolist.nvim is too buggy.
--- lua/autoload/60-visual.lua
+++ lua/autoload/60-visual.lua
@@ -22,3 +22,14 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 8
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
