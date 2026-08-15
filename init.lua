72f57f8e98810f7c2e428cd88981e17179ffa1b9
Aida van de Wetering <aida@osphire.com>
2026-08-15 17:15:04 +0200

    use a cool autoload directory

--- init.lua
+++ init.lua
@@ -1,15 +1,8 @@
--------------------------------------------------------
--- Load all the other things in specific order,
--- that's why they're numbered!
--------------------------------------------------------
-require("01-options")
-require("02-keymaps")
-require("03-autocommands")
-require("04-neovide")
--- also loads all the plugins in the plugins dir.
-require("05-lazy")
-require("06-lsp")
-require("07-spelling")
-require("08-open-in-obsidian")
-require("09-rustaceanvim-settings")
-require("50-folding")
+--- Load all of the (partial) config files in the `./lua/autoload` dir.
+--- Very much inspired by the `conf.d`-style thing.
+--- Note that Lazy.nvim is loaded this way too, and that in turn loads all the entries of `./lua/plugins`
+local files = vim.fn.glob(vim.fn.stdpath("config") .. "/lua/autoload/*.lua", false, true)
+table.sort(files)
+for _, file in ipairs(files) do
+    require("autoload." .. vim.fn.fnamemodify(file, ":t:r"))
+end
