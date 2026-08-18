223ab14a1a4982d3ff889e7a27d1a043343adadb
Aida van de Wetering <aida@osphire.com>
2026-08-18 13:14:03 +0200

    properly implement treesitter textobjects

--- lua/autoload/91-remap-square-bracket.lua
+++ lua/autoload/91-remap-square-bracket.lua
@@ -2,3 +2,8 @@
 -- Be sure this is called *after* loading the plugins- then their bindings will also automatically be changed.
 vim.keymap.set("n", "s", "]", { remap = true })
 vim.keymap.set("n", "S", "[", { remap = true })
+
+-- We also remap specifically 'go to next spelling error', because we want to have `ss` and `Ss` etc. for symbols/classes/headers.
+
+vim.keymap.set("n", "sw", "]s")
+vim.keymap.set("n", "Sw", "[s")
--- lua/plugins/treesitter-textobjects.lua
+++ lua/plugins/treesitter-textobjects.lua
@@ -1,89 +1,101 @@
+-- We define keymaps outside, because that allows us to use a nicer helper method.
+-- This should not really be lazily loaded anyway.
+local function map_select_textobject(partial_keys, capture, label)
+    vim.keymap.set({ "x", "o" }, partial_keys, function()
+        require("nvim-treesitter-textobjects.select").select_textobject(capture, "textobjects")
+    end, { desc = label })
+end
+map_select_textobject("af", "@function.outer", "Around function")
+map_select_textobject("if", "@function.inner", "Inside function")
+map_select_textobject("ac", "@class.outer", "Around class/struct")
+map_select_textobject("ic", "@class.inner", "Inside class/struct")
+
+-- Swapping text objects
+vim.keymap.set("n", "<leader>es", function()
+    require("nvim-treesitter-textobjects.swap").swap_next("@parameter.inner")
+end, { desc = "Swap with next (textobject, arg in fun)" })
+vim.keymap.set("n", "<leader>eS", function()
+    require("nvim-treesitter-textobjects.swap").swap_previous("@parameter.outer")
+end, { desc = "Swap with prev. (textobject, arg in fun)" })
+
+local function map_goto_next_start(partial_key, capture, label)
+    vim.keymap.set({ "n", "x", "o" }, "]" .. partial_key, function()
+        require("nvim-treesitter-textobjects.move").goto_next_start(capture, "textobjects")
+    end, { desc = "Goto start of next " .. label })
+end
+-- Vim does have "[m" built-in, but that is pased on syntax parse, not treesitter.
+-- What we provide here is superior.
+-- Also, I think 'f' for 'function' is more intuitive and nicer to reach than 'm' for method.
+map_goto_next_start("f", "@function.inner", "function")
+map_goto_next_start("o", { "@loop.inner", "@loop.outer" }, "loop")
+-- This overwrites the 'goto next typo' if we do not use the square bracket remapping.
+map_goto_next_start("s", "@class.inner", "class")
+
+-- And the opposite- going to the (start of) the previous one
+local function map_goto_prev_start(partial_key, capture, label)
+    vim.keymap.set({ "n", "x", "o" }, "[" .. partial_key, function()
+        require("nvim-treesitter-textobjects.move").goto_previous_start(capture, "textobjects")
+    end, { desc = "Goto start of previous " .. label })
+end
+map_goto_prev_start("f", "@function.inner", "function")
+map_goto_prev_start("s", "@class.inner", "class")
+
+-- bring you to the end of the current {function}
+local function map_goto_end_textobject(partial_key, capture, label)
+    vim.keymap.set({ "n", "x", "o" }, "]" .. partial_key, function()
+        require("nvim-treesitter-textobjects.move").goto_next_end(capture, "textobjects")
+    end, { desc = "Goto end of next " .. label })
+    -- Does not make too much sense to map 'goto_next_end',
+    -- you probably call this when you are inside a function
+    vim.keymap.set({ "n", "x", "o" }, "[" .. partial_key, function()
+        require("nvim-treesitter-textobjects.move").goto_previous_end(capture, "textobjects")
+    end, { desc = "Goto end of previous " .. label })
+end
+map_goto_end_textobject("F", "@function.outer", "function")
+map_goto_end_textobject("S", "@class.outer", "class")
+
+-- The folds setting isn't too useful, as it just jumps to something that *can be* folded.
+-- vim.keymap.set({ "n", "x", "o" }, "]z", function()
+--     require("nvim-treesitter-textobjects.move").goto_next_start("@fold", "folds")
+-- end)
+
 return {
     -- functionality like 'go to next function' or
     -- 'go to next paragraph'
+    -- See:
+    -- https://ofirgall.github.io/learn-nvim/chapters/05-text-objects.html
+    -- https://www.josean.com/posts/nvim-treesitter-and-textobjects
     "nvim-treesitter/nvim-treesitter-textobjects",
     dependencies = {},
     lazy = true,
     event = { "BufReadPre", "BufNewFile" },
     opts = {
-        textobjects = {
-            select = {
-                enable = true,
-                -- e.g. if you are in a class, also allows it to
-                -- find the next class it can find.
-                lookahead = true,
-                keymaps = {
-                    -- TODO: Update based on
-                    -- https://www.josean.com/posts/nvim-treesitter-and-textobjects
-                    -- Add combinations that can come after 'v', e.g. 'vaf'
-                    -- You can use the capture groups defined in textobjects.scm
-                    ["af"] = { query = "@function.outer", desc = "Select around function" },
-                    ["if"] = { query = "@function.inner", desc = "Select inside function" },
-                    ["al"] = { query = "@class.outer", desc = "Select around struct/class" },
-                    ["il"] = { query = "@class.inner", desc = "Select inside struct/class" },
-                    -- You can also use captures from other query groups like `locals.scm`
-                    -- ["a{"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope, e.g. {...} " },
-                },
-                -- You can choose the select mode (default is charwise 'v')
-                --
-                -- Can also be a function which gets passed a table with the keys
-                -- * query_string: eg '@function.inner'
-                -- * method: eg 'v' or 'o'
-                -- and should return the mode ('v', 'V', or '<c-v>') or a table
-                -- mapping query_strings to modes.
-                selection_modes = {
-                    ["@parameter.outer"] = "v", -- charwise
-                    ["@function.outer"] = "V", -- linewise
-                    ["@class.outer"] = "<c-v>", -- blockwise
-                },
-                -- If you set this to `true` (default is `false`) then any textobject is
-                -- extended to include preceding or succeeding whitespace.
-                include_surrounding_whitespace = true,
+        select = {
+            -- e.g. if you are in a class, also allows it to
+            -- find the next class it can find.
+            lookahead = true,
+            -- You can choose the select mode (default is charwise 'v')
+            --
+            -- Can also be a function which gets passed a table with the keys
+            -- * query_string: eg '@function.inner'
+            -- * method: eg 'v' or 'o'
+            -- and should return the mode ('v', 'V', or '<c-v>') or a table
+            -- mapping query_strings to modes.
+            selection_modes = {
+                ["@parameter.outer"] = "v", -- charwise
+                ["@function.outer"] = "V", -- linewise
+                -- ['@class.outer'] = '<c-v>', -- blockwise
             },
-            -- Swapping the arguments in a function, two entries in an array, etc
-            swap = {
-                enable = true,
-                swap_next = {
-                    ["<leader>esa"] = {
-                        query = "@parameter.inner",
-                        desc = "[S]wap [A]rgs/elements (in function, array)",
-                    },
-                    ["<leader>esf"] = { query = "@function.outer", desc = "[S]wap [F]unction with next" },
-                },
-                swap_previous = {
-                    ["<leader>esA"] = { query = "@parameter.inner", desc = "[S]wap [A]rgs/elements backwards" },
-                    ["<leader>esF"] = { query = "@function.outer", desc = "[S]wap [F]unction with previous" },
-                },
-            },
-            move = {
-                enable = true,
-                set_jumps = true, -- whether to set jumps in the jumplist
-                goto_next_start = {
-
-                    -- TODO: Update based on
-                    -- https://www.josean.com/posts/nvim-treesitter-and-textobjects
-                    ["]f"] = { query = "@function.outer", desc = "goto next function" },
-                    ["]l"] = { query = "@class.outer", desc = "goto next struct/class" },
-                    -- e.g. in rust stuff between `{}`.
-                    -- TODO: fix, does not work yet as expected.
-                    -- There is already built-in functionality for `]}`, but `]{`
-                    -- does not work.
-                    ["]s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
-                    ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
-                },
-                goto_next_end = {
-                    ["]F"] = { query = "@function.outer", desc = "end of function" },
-                    ["]L"] = { query = "@function.outer", desc = "end of class" },
-                },
-                goto_previous_start = {
-                    ["[f"] = { query = "@function.outer", desc = "Previous function" },
-                    ["[L"] = { query = "@class.outer", desc = "Next class" },
-                },
-                goto_previous_end = {
-                    ["[F"] = "@function.outer",
-                    ["[L"] = "@class.outer",
-                },
-            },
+            -- If you set this to `true` (default is `false`) then any textobject is
+            -- extended to include preceding or succeeding whitespace. Succeeding
+            -- whitespace has priority in order to act similarly to eg the built-in
+            -- `ap`.
+            --
+            -- Can also be a function which gets passed a table with the keys
+            -- * query_string: eg '@function.inner'
+            -- * selection_mode: eg 'v'
+            -- and should return true of false
+            include_surrounding_whitespace = false,
         },
     },
 }
