f58ac7f460ec769c13a00d78bf6a4d8a1d565ad5
Aida van de Wetering <aida@osphire.com>
2026-08-18 13:14:09 +0200

    use folding

--- lua/autoload/50-folding.lua
+++ lua/autoload/50-folding.lua
@@ -2,9 +2,13 @@
 -- See the treesitter config.
 
 vim.o.foldenable = true
+vim.o.foldlevel = 99
 -- Start folded at this level.
 -- High number = don't start anything folded.
-vim.o.foldlevel = 99
+vim.opt.foldlevelstart = 2
+-- If something is folded, everything inside of it might get folded too.
+-- This limits how deep that is, so you don't end up unfolding everything 100 times.
+vim.opt.foldnestmax = 4
 -- Show a little column on the left with the nesting level.
 -- If you make it wider (>1) then it will be like a contour,
 -- which is cute but takes up a lot of space.
@@ -12,7 +16,7 @@ vim.o.foldlevel = 99
 -- TODO: merge the fold column with indent blankline
 -- https://www.reddit.com/r/neovim/comments/1ag3uta/finally_the_combination_of_foldcolumn/
 
--- Customise how the folds look.
--- https://github.com/neovim/neovim/pull/20750
+-- If this is empty, then it will still be syntax highlighted
 vim.o.foldtext = ""
-vim.o.fillchars = "fold:-"
+-- The characters used to denote a fold are defined in the 'visuals' file.
+-- (Because it is a comma-separated string)
--- lua/autoload/60-visual.lua
+++ lua/autoload/60-visual.lua
@@ -37,3 +37,5 @@ vim.api.nvim_create_autocmd("TextYankPost", {
         vim.highlight.on_yank()
     end,
 })
+
+vim.o.fillchars = "fold:-"
